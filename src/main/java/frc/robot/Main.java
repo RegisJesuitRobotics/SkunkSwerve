1923d2b501c219a44f189ab6df3ea3015fddda2c
ohowe <ohowe@protonmail.com>
2022-04-01 17:13:35 -0600

    Add workflows

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -13,7 +13,8 @@ import edu.wpi.first.wpilibj.RobotBase;
  * the parameter class to the startRobot call.
  */
 public final class Main {
-    private Main() {}
+
+ private Main() {}
 
     /**
      * Main initialization method. Do not perform any initialization here.
